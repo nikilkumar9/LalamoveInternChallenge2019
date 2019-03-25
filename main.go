d21ba5b495d433536e77f61cb160335e8e7ec0f8
Nikil Kumar <kumarnikil9865@gmail.com>
2019-03-25 23:23:12 +0800

    input file included & main.go file updated(not working properly)

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/coreos/go-semver/semver"
 	"github.com/google/go-github/github"
 )
 
+func reverse(releases []*semver.Version) []*semver.Version {
+	for i := 0; i < len(releases)/2; i++ {
+		j := len(releases) - i - 1
+		releases[i], releases[j] = releases[j], releases[i]
+	}
+	return releases
+}
+
 // LatestVersions returns a sorted slice with the highest version as its first element and the highest version of the smaller minor versions in a descending order
 func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*semver.Version {
 	var versionSlice []*semver.Version
 	// This is just an example structure of the code, if you implement this interface, the test cases in main_test.go are very easy to run
+
+	// Sorting array of version numbers by ascending order and reversing the order
+	semver.Sort(releases)
+	releases = reverse(releases)
+
+	//Removing versions that are lower than minimum
+	for i := 0; i < len(releases); i++ {
+		if !(releases[i].LessThan(*minVersion)) {
+			versionSlice = append(versionSlice, releases[i])
+		} else {
+			break
+		}
+	}
+
+	// Remove Duplicate copies (code not complete)
+
 	return versionSlice
 }
 
@@ -23,21 +50,45 @@ func main() {
 	// Github
 	client := github.NewClient(nil)
 	ctx := context.Background()
-	opt := &github.ListOptions{PerPage: 10}
-	releases, _, err := client.Repositories.ListReleases(ctx, "kubernetes", "kubernetes", opt)
-	if err != nil {
-		panic(err) // is this really a good way?
-	}
-	minVersion := semver.New("1.8.0")
-	allReleases := make([]*semver.Version, len(releases))
-	for i, release := range releases {
-		versionString := *release.TagName
-		if versionString[0] == 'v' {
-			versionString = versionString[1:]
-		}
-		allReleases[i] = semver.New(versionString)
-	}
-	versionSlice := LatestVersions(allReleases, minVersion)
+	opt := &github.ListOptions{PerPage: 100}
 
-	fmt.Printf("latest versions of kubernetes/kubernetes: %s", versionSlice)
+	//Opening file and getting names
+	file, err := os.Open("file.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+	file.Close()
+
+	txtlines = txtlines[1:] //Skip header line
+	for _, line := range txtlines {
+		software := strings.Split(line, ",")[0]
+		minVal := strings.Split(line, ",")[1]
+		individual := strings.Split(software, "/")
+
+		releases, _, err := client.Repositories.ListReleases(ctx, individual[0], individual[1], opt)
+		if err != nil {
+			panic(err) // is this really a good way?
+		}
+
+		minVersion := semver.New(minVal)
+		allReleases := make([]*semver.Version, len(releases))
+		for _, release := range releases {
+			versionString := *release.TagName
+			if versionString[0] == 'v' {
+				versionString = versionString[1:]
+			}
+			versionString = strings.Split(versionString, "-")[0]
+
+		}
+		versionSlice := LatestVersions(allReleases, minVersion)
+
+		fmt.Printf("latest versions of kubernetes/kubernetes: %s", versionSlice)
+	}
 }
